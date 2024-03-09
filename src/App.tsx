6b69355c57095256275f9d8e47713c7858f3784d
broncogooch <calebgooch2000@gmail.com>
2024-03-08 22:51:19 -0700

    Getting welcome to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import jsonData from ;
+
 import './App.css';
 
+function Welcome() {
+  return <h1>Welcome to March Madness! </h1>;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+      <Welcome />
+  </div>;
 }
 
 export default App;
